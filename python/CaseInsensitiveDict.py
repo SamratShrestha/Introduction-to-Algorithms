12d1e2a3b8fc83f7c82c4352561e36e5f66f0e41
SamratShrestha <samratshrestha4536@gmail.com>
2022-01-01 22:09:30 +0545

    add return type to caseinsensitivedict

--- python/CaseInsensitiveDict.py
+++ python/CaseInsensitiveDict.py
@@ -5,18 +5,20 @@ class CaseInsensitiveDict(UserDict):
     def __setitem__(self,key:str,value:Any):
         return super().__setitem__(key.lower(),value)
 
-    def __getitem__(self,key:str):
+    def __getitem__(self,key:str)->Any:
         return super().__getitem__(key.lower())
 
-    def __delitem__(self,key:str):
+    def __delitem__(self,key:str)->Any:
         return super().__delitem__(key.lower())
 
 
 
-headers = CaseInsensitiveDict({
-    'Content-Type': 'application/json',
-    'Content-Length': 10
-})
+if __name__ == "__main__":
+    headers = CaseInsensitiveDict({
+        'Content-Type': 'application/json',
+        'Content-Length': 10
+    })
 
-print(headers['CONTENT-TYPE'])
-print(headers['content-length'])
+    print(headers['CONTENT-TYPE'])
+    print(headers['content-length'])
+    del headers
