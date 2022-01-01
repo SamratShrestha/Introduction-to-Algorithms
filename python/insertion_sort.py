8f21098760f29010eb1dd01fb0e8828f85bcf654
SamratShrestha <samratshrestha4536@gmail.com>
2022-01-01 21:15:30 +0545

    reverse insertion sort

--- python/insertion_sort.py
+++ python/insertion_sort.py
@@ -1,4 +1,4 @@
-arr = [1000,2,413,412,41,2,1]
+arr = [1, 2, 2, 41, 412, 413, 1000]
 
 def insertion_sort(arr):
     for i in range(1,len(arr)):
@@ -8,8 +8,18 @@ def insertion_sort(arr):
             arr[j+1] = arr[j]
             j = j - 1
         arr[j+1] = key
-        print(arr)
+    return arr
+
+def insertion_sort_rev(arr):
+    for i in range(1,len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key > arr[j]:
+            arr[j+1] = arr[j]
+            j = j - 1
+        arr[j+1] = key
     return arr
 
 if __name__ == '__main__':
     print(insertion_sort(arr))
+    print(insertion_sort_rev(arr))
